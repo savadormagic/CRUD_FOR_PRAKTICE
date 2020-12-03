c213f5458f66cf5ba1d1a47d7e9004b2c2addc58
sara <saveliy.ivonin@gmail.com>
2020-12-04 01:30:20 +0500

    first working CRUD API

---
+++ src/main/java/com/example/hopeFinal/HopeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hopeFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeFinalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/hopeFinal/model/User.java
@@ -0,0 +1,16 @@
+package com.example.hopeFinal.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+}
